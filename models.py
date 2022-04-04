b383c602b0b9b20ad2945ee080a169cc0a898ac2
Mad Neutrino <madneutrino@jezer0x.com>
2022-04-04 21:27:58 +0800

    refactor staked amounts to track when the asset was staked

--- models.py
+++ models.py
@@ -18,4 +18,9 @@ class SingleStaker:
     lp_ratio: float
     staked: float
     
-    
+
+@dataclass
+class LPStake:
+    id: int # the id of this stake
+    shares: float # shares owned from the pool. Equal to the input value of the stake
+    step_last_added: int # step on which the stake was last added for ILP calculation.

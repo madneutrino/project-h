9e9c61b3c603885c6e6fb33ea419afa05d6a564f
Mad Neutrino <madneutrino@jezer0x.com>
2022-04-07 14:25:38 +0800

    split asset vault into funds. And only LP from the latest fund. this way profits arent re-invested.

--- models.py
+++ models.py
@@ -1,11 +1,6 @@
 from dataclasses import dataclass
 
 @dataclass
-class Stake:
-    value: float
-    shares: float
-        
-@dataclass
 class LiquidityPool:
     token: str
     value: float
@@ -24,3 +19,10 @@ class LPStake:
     id: int # the id of this stake
     shares: float # shares owned from the pool. Equal to the input value of the stake
     step_last_added: int # step on which the stake was last added for ILP calculation.
+
+@dataclass
+class Fund:
+    token: str
+    value: float
+    lp_stake: LPStake
+    
